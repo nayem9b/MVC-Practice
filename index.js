5642925a7d770ce60a8d151db4f02880b7855b9d
nayem9b <razwanniam1@gmail.com>
2022-12-16 21:37:43 +0600

    uncommented

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 const products_routes = require("./Routes/Products");
-// mongoose
-//   .connect("mongodb://localhost:27017/nayemapi")
-//   .then(() => console.log("Connection Successful"))
-//   .then((err) => console.log(err));
+mongoose
+  .connect("mongodb://localhost:27017/nayemapi")
+  .then(() => console.log("Connection Successful"))
+  .then((err) => console.log(err));
 // Middlewere
 app.use(cors());
 app.use("/api/products", products_routes);
